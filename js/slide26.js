f1789a66c443fffaee03de7cec676819a75d436c
bwcsemaj <bwc_semaj@live.com>
2020-06-30 15:47:46 -0400

    fixed slide 26 so it doesn't take forever to do

--- js/slide26.js
+++ js/slide26.js
@@ -1,13 +1,32 @@
-for(var index = 0; 59 > index; index++){
-    console.log("" + index + " " + fibonacci(index));
+for(var index = 0; 59 >= index; index++){
+    console.log("" + index + " " + fibonacci2(index));
 }
 
-function fibonacci(value){
+// function fibonacci(value){
+//     if(value == 0){
+//         return 0;
+//     } else if(value == 1){
+//         return 1;
+//     }
+
+//     return fibonacci(value - 1) + fibonacci(value - 2);
+// }
+
+function fibonacci2(value){
     if(value == 0){
         return 0;
     } else if(value == 1){
         return 1;
     }
-
-    return fibonacci(value - 1) + fibonacci(value - 2);
+    
+    //Cycle through to the nth position
+    var start = 0;
+    var end = 1;
+    var answer = 1;
+    for(var index = 2; value >= index; index++){
+        answer = start + end;
+        start = end;
+        end = answer;
+    }
+    return answer;
 }
